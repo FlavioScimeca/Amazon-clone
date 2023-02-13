47d10d6ade4c429e9e08ca8d9c9e2773fb0f4b35
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-02-13 23:50:31 +0100

    created next app & configured Tailwind CSS

---
+++ amazon-clone/src/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Amazon | clone</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2 className="text-2xl bg-red-600 font-extrabold">ciao</h2>
+      </main>
+    </>
+  );
+}
