b1b0ba01d2f529121b31320a9da002434aee3b11
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-02-16 16:14:31 +0100

    added webhook with STRIPE using Micro and Firebase-admin, completed success page

--- amazon-clone/src/pages/api/hello.js
+++ amazon-clone/src/pages/api/hello.js
@@ -1,5 +1,74 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { buffer } from 'micro';
+import * as admin from 'firebase-admin';
+
+//Secure a connection to FIREBASE from backend
+const serviceAccount = require('../../../permission.json');
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+// Establish connection to Stripe
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fullfillOrder = async () => {
+  console.log('fullfilling order');
+
+  return app
+    .firestore()
+    .collection('user')
+    .doc(session.metadata.email)
+    .collection('orders')
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: JSON.parse(session.metadata.images),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SECCESS: Order ${session.id} has been added to the DB`);
+    });
+};
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig = req.headers['stripe-signature'];
+
+    let event;
+    // Verify that the EVENT posted came from stripe
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log('Error', err.message);
+      return res.status(400).send(`Webhook error ${err.message}`);
+    }
+    // Handle to checkout.session.completed event
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+      //Fullfil the order
+      return fullfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error ${err.message}`));
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
---
+++ amazon-clone/src/pages/orders.js
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div>
+      <Header />
+      <h2>Orders</h2>
+    </div>
+  );
+};
+
+export default Orders;
